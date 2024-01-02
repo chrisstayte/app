dee79d4daa6656ea2a556e1e9d9917eb5857660c
Chris Stayte <ctstayte@gmail.com>
2024-01-02 13:33:59 -0500

    test dev build

---
+++ src/app/components/footer.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div className='bg-black text-white justify-center '>
+      <p>Footer</p>
+    </div>
+  );
+}
---
+++ src/app/components/navbar.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div className='bg-[#645DE0] justify-center p-4 '>
+      <p>Nav Bar</p>
+      <div></div>
+    </div>
+  );
+}
